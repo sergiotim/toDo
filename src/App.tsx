469aaa1ee084e817dd11ae5df2e8dde3bd1b2e05
Sérgio Timoteo <74915883+sergiotim@users.noreply.github.com>
2025-04-06 00:54:46 -0300

    Funcionalidade deletar task aplicada

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,91 @@
-import './App.css'
-import { Navbar,Container, Stack } from 'react-bootstrap'
-import {Task} from './components/tasks'
+import "./App.css";
+import {
+  Navbar,
+  Container,
+  Stack,
+  Form,
+  Button,
+  InputGroup,
+} from "react-bootstrap";
+import { Task } from "./components/tasks";
+import { useState } from "react";
 
-function App() {
-
-
-
-  
-  return (
-    <>
-      <div className="gap-3">
-        <Navbar className='bg-primary mb-4 h-100'>
-          <Container fluid="md justify-content-center">
-            <Navbar.Brand className='text-light fs-1' href='#'>ToDo</Navbar.Brand>
-          </Container>
-        </Navbar>
-        <Container fluid="md">
-          <Stack gap={2}>
-            <Task
-              nameTask='comer banana'
-            />
-            <Task
-              nameTask='comer banana'
-            />
-            <Task
-              nameTask='comer banana'
-            />
-          </Stack>
-        
-        </Container>
-      </div>
-    </>
-  )
+export interface ITask {
+  id: string;
+  nameTask: string;
+  isChecked: boolean;
 }
 
-export default App
+function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [nameNewTask, setNameNewTask] = useState<string>("");
+
+  function addTask(nameNewTask: string) {
+    const newTask: ITask = {
+      id: crypto.randomUUID(),
+      nameTask: nameNewTask,
+      isChecked: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    console.log(tasks);
+    setNameNewTask("");
+  }
+
+  function deleteTask(id: string) {
+    setTasks((e) => e.filter((task) => task.id != id));
+  }
+
+  return (
+    <>
+      <Navbar className="bg-primary mb-4 h-100">
+        <Container fluid="md justify-content-center">
+          <Navbar.Brand className="text-light fs-1" href="#">
+            ToDo
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container fluid="md">
+        <InputGroup className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Nome da Tarefa"
+            onChange={(e) => setNameNewTask(e.target.value)}
+            onKeyDown={(e) => {
+              if (nameNewTask != "") {
+                if (e.key === "Enter") {
+                  addTask(nameNewTask);
+                }
+              }
+            }}
+            value={nameNewTask}
+          ></Form.Control>
+          <Button
+            onClick={() => {
+              if (nameNewTask != "") {
+                addTask(nameNewTask);
+              }
+            }}
+          >
+            <i className="bi bi-plus-lg"></i>
+          </Button>
+        </InputGroup>
+
+        <Stack gap={2}>
+          {tasks.map((task) => (
+            <Task
+              key={task.id}
+              id={task.id}
+              nameTask={task.nameTask}
+              isChecked={task.isChecked}
+              deleteTask={deleteTask}
+            ></Task>
+          ))}
+        </Stack>
+      </Container>
+    </>
+  );
+}
+
+export default App;
